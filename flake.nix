ab65dc1809141b9c946c9437f6f3f865ed58f388
sahiti.c <sahiti.c@media.net>
2023-02-02 20:44:03 +0530

    add hive package to gateWwyRole + formatting

--- flake.nix
+++ flake.nix
@@ -105,10 +105,15 @@
                 isSystemUser = true;
                 group = "hadoop";
               };
+              environment.systemPackages = [
+                self.defaultPackage.${config.nixpkgs.system}
+              ];
             })
 
             (mkIf cfg.enable {
-              environment.systemPackages = [ self.defaultPackage.${config.nixpkgs.system} ];
+              environment.systemPackages = [
+                self.defaultPackage.${config.nixpkgs.system}
+              ];
               networking.firewall.allowedTCPPorts = (mkIf cfg.openFirewall [ 10000 10001 10002 14000 ]) // (mkIf cfg.metastore.openFirewall [ 9083 ]);
 
               users.users.hive = {
@@ -150,24 +155,24 @@
                   wantedBy = [ "multi-user.target" ];
                   path = [ pkgs.hadoop pkgs.sudo config.krb5.kerberos ];
                   script = with pkgs; ''
-# in future to be escaped with a kerberos enable option
-# kinit -k -t /var/security/keytab/hiveserver.service.keytab hive/hiveserver
+                    # in future to be escaped with a kerberos enable option
+                    # kinit -k -t /var/security/keytab/hiveserver.service.keytab hive/hiveserver
 
-sudo -u hdfs hadoop fs -mkdir -p /home/hive || true
-sudo -u hdfs hadoop fs -chown hive:hadoop /home/hive || true
+                    sudo -u hdfs hadoop fs -mkdir -p /home/hive || true
+                    sudo -u hdfs hadoop fs -chown hive:hadoop /home/hive || true
 
-sudo -u hdfs hadoop fs -mkdir /tmp || true
-sudo -u hdfs hadoop fs -chown hdfs:hadoop /tmp || true
-sudo -u hdfs hadoop fs -chmod g+w /tmp || true
+                    sudo -u hdfs hadoop fs -mkdir /tmp || true
+                    sudo -u hdfs hadoop fs -chown hdfs:hadoop /tmp || true
+                    sudo -u hdfs hadoop fs -chmod g+w /tmp || true
 
-sudo -u hdfs hadoop fs -mkdir -p /user/hive || true
-sudo -u hdfs hadoop fs -chown hive:hadoop /user/hive || true
+                    sudo -u hdfs hadoop fs -mkdir -p /user/hive || true
+                    sudo -u hdfs hadoop fs -chown hive:hadoop /user/hive || true
 
-sudo -u hdfs hadoop fs -mkdir /user/hive/warehouse || true
-sudo -u hdfs hadoop fs -chmod g+w /user/hive/warehouse || true
+                    sudo -u hdfs hadoop fs -mkdir /user/hive/warehouse || true
+                    sudo -u hdfs hadoop fs -chmod g+w /user/hive/warehouse || true
 
-${pkgs.coreutils}/bin/mkdir /var/run/hive || true
-${pkgs.coreutils}/bin/chown hive:hadoop /var/run/hive || true
+                    ${pkgs.coreutils}/bin/mkdir /var/run/hive || true
+                    ${pkgs.coreutils}/bin/chown hive:hadoop /var/run/hive || true
 
                   '';
                   serviceConfig = {
