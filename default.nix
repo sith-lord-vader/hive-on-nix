911c707cd994888f355deb4c95a306d3150fc56a
sahiti.c <sahiti.c@media.net>
2023-04-13 17:01:10 +0530

    test for which mysql_jdbc jar is present for compat reasons

--- default.nix
+++ default.nix
@@ -21,7 +21,9 @@ stdenv.mkDerivation rec {
         # mkdir -p $out/{share,bin,lib}
 				mkdir $out
         mv * $out/
-        cp ${mysql_jdbc}/share/java/mysql-connector-java.jar $out/lib
+        test -f ${mysql_jdbc}/share/java/mysql-connector-java.jar && \
+         cp ${mysql_jdbc}/share/java/mysql-connector-java.jar $out/lib || \
+         cp ${mysql_jdbc}/share/java/mysql-connector-j.jar $out/lib || 
 
 				for n in $(find $out{,/hcatalog}/bin -type f ! -name "*.*"); do
           wrapProgram "$n" \
@@ -30,7 +32,7 @@ stdenv.mkDerivation rec {
 						--set-default HADOOP_HOME "${hadoop}/lib/${hadoop.untarDir}" \
             --prefix PATH : "${lib.makeBinPath [ bash coreutils which gawk psutils ]}" \
 						--prefix JAVA_LIBRARY_PATH : "${lib.makeLibraryPath [ mysql_jdbc ]}" \
-						--suffix HIVE_AUX_JARS_PATH , "${mysql_jdbc}/share/java/mysql-connector-java.jar"
+						--suffix HIVE_AUX_JARS_PATH , "$out/lib/mysql-connector-java.jar"
 				done
 	'';
 }
